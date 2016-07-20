dad9181301b34fa130f94b783a8e32563d83e5d7
wheeler36 <wheeler.hord@gmail.com>
2016-07-19 22:54:17 -0800

    Add CRUD abilities and add views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,49 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
+
   def index
+    @jobs = Job.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company, :url)
   end
 end
