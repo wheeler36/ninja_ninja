8ef7094a66deaa640b50f91f5c62dee992b8d86c
wheeler36 <wheeler.hord@gmail.com>
2016-07-23 13:22:40 -0800

    More styles and figaro gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.configure do
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  end
 end
